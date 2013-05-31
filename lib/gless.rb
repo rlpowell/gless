db60fa7e2affea6e5bfb02e78ff644698f36d9f2
Byron Johnson <ByronJohnsonFP@gmail.com>
2013-05-31 14:08:31 -0700

    Bump version to 1.2.0 from 1.1.3.

--- lib/gless.rb
+++ lib/gless.rb
@@ -6,7 +6,7 @@
 # project.
 module Gless
   # The current version number.
-  VERSION = '1.1.3'
+  VERSION = '1.2.0'
 
   # Sets up the config, logger and browser instances, the ordering
   # of which is slightly tricky.
